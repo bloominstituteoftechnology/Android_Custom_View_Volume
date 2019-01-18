c0364a0d1376781bca84e5135ebfa1f48e07c2a9
Jacob Seppanen <jbseppanen@gmail.com>
2019-01-17 20:04:13 -0800

    Now actually controls volume.

--- app/src/main/java/com/example/android_custom_view_volume/views/VolumeControlView.java
+++ app/src/main/java/com/example/android_custom_view_volume/views/VolumeControlView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.media.AudioManager;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -13,12 +14,13 @@ import com.example.android_custom_view_volume.R;
 
 public class VolumeControlView extends View {
     private static final String TAG = "Logging";
+    public static final int MAX_ANGLE = 270;
     protected Paint outerCirclePaint, innerCirclePaint, knobCirclePaint;
-    protected int outerColor, innerColor, knobColor;
-    protected int rotationAngle;
-    float centerX, centerY, outerRadius, innerRadius, knobRadius;
+    protected int outerColor, innerColor, knobColor, rotationAngle, volumeIncrement;
+    float centerX, centerY, outerRadius, innerRadius, knobRadius, knobDistanceFromCenter;
     double startRotationAngle;
     boolean clockwise, atBottomStop, atTopStop;
+    AudioManager audioManager;
 
 
     public VolumeControlView(Context context) {
@@ -45,7 +47,12 @@ public class VolumeControlView extends View {
         outerCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         innerCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         knobCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        rotationAngle = 0;
+
+        audioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+        int streamMaxVolume = audioManager.getStreamMaxVolume(audioManager.STREAM_MUSIC);
+        volumeIncrement = MAX_ANGLE / streamMaxVolume;
+        rotationAngle = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) * volumeIncrement;
+
 
         if (attrs != null) {
             TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.VolumeControlView);
@@ -59,12 +66,14 @@ public class VolumeControlView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         centerX = getWidth() / 2f;
         centerY = getHeight() / 2f;
         outerRadius = (centerX < centerY ? centerX : centerY) * 0.9f;
         innerRadius = (centerX < centerY ? centerX : centerY) * 0.75f;
-        float knobDistanceFromCenter = (centerX < centerY ? centerX : centerY) * .55f;
+        knobDistanceFromCenter = (centerX < centerY ? centerX : centerY) * .55f;
         knobRadius = knobDistanceFromCenter * 0.2f;
+
 
         outerCirclePaint.setColor(getResources().getColor(outerColor));
         innerCirclePaint.setColor(getResources().getColor(innerColor));
@@ -76,6 +85,9 @@ public class VolumeControlView extends View {
         float knobCenterX = (float) (centerX - (knobDistanceFromCenter / Math.sqrt(2)));
         float knobCenterY = (float) (centerY + (knobDistanceFromCenter / Math.sqrt(2)));
         canvas.drawCircle(knobCenterX, knobCenterY, knobRadius, knobCirclePaint);
+
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, rotationAngle / (volumeIncrement), 0);
+
     }
 
     @Override
@@ -99,16 +111,16 @@ public class VolumeControlView extends View {
                 startRotationAngle = newRotationAngle;
                 if (!((clockwise && atTopStop) || (!clockwise && atBottomStop))) {
                     rotationAngle = (int) (differenceAngle + rotationAngle);
-                if (rotationAngle > 270) {
-                    rotationAngle = 270;
-                    atBottomStop = true;
-                } else if (rotationAngle < 0) {
-                    rotationAngle = 0;
-                    atTopStop = true;
-                } else {
-                    atBottomStop = false;
-                    atTopStop = false;
-                }
+                    if (rotationAngle > MAX_ANGLE) {
+                        rotationAngle = MAX_ANGLE;
+                        atBottomStop = true;
+                    } else if (rotationAngle < 0) {
+                        rotationAngle = 0;
+                        atTopStop = true;
+                    } else {
+                        atBottomStop = false;
+                        atTopStop = false;
+                    }
                     invalidate();
                 }
                 break;
