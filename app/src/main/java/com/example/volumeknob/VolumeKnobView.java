7d33661cd7a2307a42dee71e930157c34bd7d870
BL-CoderOvertime <brandonlively@live.com>
2019-04-11 16:29:59 -0400

    setup rotation functionality

--- app/src/main/java/com/example/volumeknob/VolumeKnobView.java
+++ app/src/main/java/com/example/volumeknob/VolumeKnobView.java
@@ -6,11 +6,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class VolumeKnobView extends View {
-    Paint paint,paintSmallCircle;
-    float x, y, radius,smallX,smallY,smallRadius;
+    Paint paint, paintSmallCircle;
+    float x, y, radius, smallX, smallY, smallRadius, touchX, rotation;
+
     public VolumeKnobView(Context context) {
         super(context);
         init();
@@ -31,7 +33,23 @@ public class VolumeKnobView extends View {
         init();
     }
 
-    private void init(){
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchX = event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float diffX = event.getX() - touchX;
+                rotation += diffX / 3;
+                touchX = event.getX();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+    private void init() {
         paint = new Paint();
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.FILL);
@@ -48,15 +66,16 @@ public class VolumeKnobView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        x = getWidth()/2;
-        y = getHeight()/2;
-        smallX = (float) (x-(x/2.5));
-        radius = getWidth()/4;
+        x = getWidth() / 2;
+        y = getHeight() / 2;
+        smallX = (float) (x - (x / 2.5));
+        radius = getWidth() / 4;
         smallY = y;
-        smallRadius = radius/16;
+        smallRadius = radius / 16;
 
-        canvas.drawCircle(x,y,radius,paint);
-        canvas.drawCircle(smallX,smallY,smallRadius,paintSmallCircle);
+        canvas.rotate(rotation,x,y);
+        canvas.drawCircle(x, y, radius, paint);
+        canvas.drawCircle(smallX, smallY, smallRadius, paintSmallCircle);
 
         super.onDraw(canvas);
     }
