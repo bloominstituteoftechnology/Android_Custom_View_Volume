06438688529065a972154b83141c9af38d9fb53c
BL-CoderOvertime <brandonlively@live.com>
2019-04-11 18:53:43 -0400

    allDone! added toast to display rotation position on 0-100 scale, added funcationality to programatically change the colors of the knob and indicator.

--- app/src/main/java/com/example/volumeknob/MainActivity.java
+++ app/src/main/java/com/example/volumeknob/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.volumeknob;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VolumeKnobView vkv;
+        vkv = findViewById(R.id.volume_knob);
+        vkv.setIndicatorColor(Color.CYAN);
+        vkv.setKnobColor(Color.GRAY);
+
     }
 }
