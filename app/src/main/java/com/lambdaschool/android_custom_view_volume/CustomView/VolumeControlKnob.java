ce45cbd5c9553d09f67df2ea47fc90c870c442b1
heiligbasil <basilief@yahoo.com>
2019-04-11 17:07:10 -0700

    Got the volume control knob functional with a min and max

--- app/src/main/java/com/lambdaschool/android_custom_view_volume/CustomView/VolumeControlKnob.java
+++ app/src/main/java/com/lambdaschool/android_custom_view_volume/CustomView/VolumeControlKnob.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.android_custom_view_volume.CustomView;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -8,8 +9,19 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+
+import com.lambdaschool.android_custom_view_volume.R;
 
 public class VolumeControlKnob extends View {
+    Paint paintBottom;
+    Paint paintMiddle;
+    Paint paintTop;
+    Paint paintKnob;
+    boolean knobMoving;
+    float rotation;
+    float eventX;
+    float eventY;
 
     public VolumeControlKnob(Context context) {
         super(context);
@@ -32,8 +44,25 @@ public class VolumeControlKnob extends View {
     }
 
     protected void init(AttributeSet attrs) {
-        if (attrs != null) {
+        paintBottom = new Paint();
+        paintBottom.setColor(Color.BLACK);
 
+        paintMiddle = new Paint();
+        paintMiddle.setColor(Color.RED);
+
+        paintTop = new Paint();
+        paintTop.setColor(Color.YELLOW);
+
+        paintKnob = new Paint();
+        paintKnob.setColor(Color.GRAY);
+
+        if (attrs != null) {
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.VolumeControlKnob);
+            paintKnob.setStrokeWidth(typedArray.getFloat(R.styleable.VolumeControlKnob_line_thickness, 2));
+            paintKnob.setColor(typedArray.getColor(R.styleable.VolumeControlKnob_line_color, Color.DKGRAY));
+            paintKnob.setAntiAlias(typedArray.getBoolean(R.styleable.VolumeControlKnob_anti_aliasing, true));
+
+            typedArray.recycle();
         }
     }
 
@@ -41,15 +70,19 @@ public class VolumeControlKnob extends View {
     protected void onDraw(Canvas canvas) {
         float canvasCenterX = getWidth() / 2f;
         float canvasCenterY = getHeight() / 2f;
-        float canvasMaxRadius = (canvasCenterX < canvasCenterY ? canvasCenterX : canvasCenterY) - 25;
+        float canvasMaxRadius = (canvasCenterX < canvasCenterY ? canvasCenterX : canvasCenterY) * .98f;
 
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-        canvas.drawCircle(canvasCenterX, canvasCenterY, canvasMaxRadius, paint);
-        paint.setColor(Color.RED);
-        canvas.drawCircle(canvasCenterX, canvasCenterY, canvasMaxRadius - 5, paint);
-        paint.setColor(Color.YELLOW);
-        canvas.drawCircle(canvasCenterX, canvasCenterY, canvasMaxRadius - 25, paint);
+        //canvas.drawText(String.valueOf(rotation),100,100,paintBottom);
+        //TextView textView = ((TextView) findViewById(R.id.text_view));
+        //textView.setText(String.valueOf(rotation));
+        //(getResources().getAssets())
+        canvas.rotate(rotation, canvasCenterX, canvasCenterY);
+
+
+        canvas.drawCircle(canvasCenterX, canvasCenterY, canvasMaxRadius, paintBottom);
+        canvas.drawCircle(canvasCenterX, canvasCenterY, canvasMaxRadius * .98f, paintMiddle);
+        canvas.drawCircle(canvasCenterX, canvasCenterY, canvasMaxRadius * .91f, paintTop);
+        canvas.drawCircle(canvasCenterX * .62f, canvasCenterY * .62f, canvasMaxRadius * .11f, paintKnob);
 
 
         super.onDraw(canvas);
@@ -61,16 +94,32 @@ public class VolumeControlKnob extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: // triggered when view is touched
                 // get and store start point with event.getX()
+                eventX = event.getX();
+                eventY = event.getY();
+                knobMoving = true;
+
                 break;
+
             case MotionEvent.ACTION_MOVE: // triggered after ACTION_DOWN but when touch is moved
                 // get end point and calculate total distance traveled
                 // use the total distance traveled to calculate the desired change in rotation
                 // apply that change to your rotation variable
                 // you may want to use a minimum and maximum rotation value to limit the rotation
                 // use the new rotation to convert to the desired volume setting
+                float distanceX = eventX - event.getX();
+                float distanceY = eventY - event.getY();
+                rotation = distanceX + distanceY / 2;
+
+                if (rotation < -100)
+                    rotation = -100;
+                if (rotation > 175)
+                    rotation = 175;
+
                 invalidate(); // this will cause the onDraw method to be called again with your new values
                 break;
+
             case MotionEvent.ACTION_UP: // triggered when touch ends
+                invalidate();
                 break;
         }
 
--- app/src/main/java/com/lambdaschool/android_custom_view_volume/MainActivity.java
+++ app/src/main/java/com/lambdaschool/android_custom_view_volume/MainActivity.java
@@ -2,6 +2,7 @@ package com.lambdaschool.android_custom_view_volume;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
