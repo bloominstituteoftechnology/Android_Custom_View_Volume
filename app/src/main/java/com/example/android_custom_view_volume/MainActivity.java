ff4f39fb52ff0fb05f340ccd0c0010e5c0a7e18a
Shoonsk8 <shoon@att.net>
2019-04-19 07:45:15 -0400

    Obtaining Percent

--- app/src/main/java/com/example/android_custom_view_volume/MainActivity.java
+++ app/src/main/java/com/example/android_custom_view_volume/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android_custom_view_volume;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final DialView gv=findViewById(R.id.dial_volume);
+
+        gv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final TextView tv=findViewById(R.id.text_values);
+                tv.setText(gv.getValues());
+                tv.invalidate();
+
+            }
+        });
+
+
+
     }
 }
