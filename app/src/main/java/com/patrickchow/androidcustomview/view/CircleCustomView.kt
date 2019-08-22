b14a701d569b6f8993f15823cc0d05b837d5fa75
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-22 16:02:23 -0400

    Added a basic Circle Custom View

--- app/src/main/java/com/patrickchow/androidcustomview/view/CircleCustomView.kt
+++ app/src/main/java/com/patrickchow/androidcustomview/view/CircleCustomView.kt
@@ -1,9 +1,12 @@
 package com.patrickchow.androidcustomview.view
 
 import android.content.Context
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class CircleCustomView (context: Context?, attrs: AttributeSet?): View(context, attrs){
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
 }
