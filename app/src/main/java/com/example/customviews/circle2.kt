829a54f602b3087c2ab6871e9dfd7bbe48f8d895
jDanielDetes <jdanieldetes@gmail.com>
2019-08-22 16:41:43 -0400

    adjusted circle layout

--- app/src/main/java/com/example/customviews/circle2.kt
+++ app/src/main/java/com/example/customviews/circle2.kt
@@ -4,17 +4,16 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-
 import android.util.AttributeSet
 import android.view.View
 
-class circle (context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class circle2 (context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private val paint = Paint()
 
 
     override fun onDraw(canvas: Canvas?) {
-        paint.color = Color.BLACK
-       canvas?.drawCircle(width/2.0f,height/2f,500f,paint)
+        paint.color= Color.BLUE
+        canvas?.drawCircle(width/2.0f,height/2f,75f,paint)
         super.onDraw(canvas)
     }
 }
