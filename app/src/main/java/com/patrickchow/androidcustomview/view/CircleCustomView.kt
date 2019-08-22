090beb53c3fd9f0960f47a4d064f23c24b75a84e
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-22 16:01:19 -0400

    Patrick Chow: Android Custom View Initial Commit

---
+++ app/src/main/java/com/patrickchow/androidcustomview/view/CircleCustomView.kt
@@ -0,0 +1,9 @@
+package com.patrickchow.androidcustomview.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CircleCustomView (context: Context?, attrs: AttributeSet?): View(context, attrs){
+
+}
