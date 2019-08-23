2beba3246fae0fba7e196d690b8a65a7ddf2250b
ronniedipple <ronniedipple@gmail.com>
2019-08-23 01:16:51 -0500

    Bitmap images removed, replaced with canvas drawn images

--- app/src/main/java/com/example/android_custom_view_volume/activity/activity/view/CustomView.kt
+++ app/src/main/java/com/example/android_custom_view_volume/activity/activity/view/CustomView.kt
@@ -2,8 +2,14 @@ package com.example.android_custom_view_volume.activity.activity.view
 
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
@@ -11,30 +17,85 @@ import com.example.android_custom_view_volume.R
 
 
 class CustomView(context: Context, attrs: AttributeSet?):
-    LinearLayout(context, attrs){
+    View(context, attrs){
 
-    constructor(context: Context): this(context, null)
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    private var startX: Float = 0f
+    private var startY: Float = 0f
+    private var diffX: Float = 0f
+    private var diffY: Float = 0f
+    private var endPoint: Float = 0f
+    private var rotate: Float = 0f
+    private var position = 0f
+    private var maxRotation = 360f
+    private var minRotation = 0f
+    private var volume = 0f
 
     init {
-
-        val view = LayoutInflater.from(context)
-            .inflate(R.layout.customeview_layout, this, false) as ImageView
-
-        view.setImageDrawable(ContextCompat
-            .getDrawable(context, R.drawable.blackvolume))
-
-        val redView = ImageView(context)
-        redView.setImageDrawable(ContextCompat
-            .getDrawable(context, R.drawable.smallredcircle))
-
-
-        orientation = VERTICAL
-
-        addView(view)
-        addView(redView)
 
     }
 
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when(event?.action){
+
+            MotionEvent.ACTION_DOWN->{
+                startX = event.x
+
+
+
+            }
+
+            MotionEvent.ACTION_MOVE ->{
+                endPoint = event.x
+                diffX = endPoint-startX
+                rotate = position + (diffX + 360) / width
+
+
+
+                if (rotate >= maxRotation){
+                    rotate = maxRotation
+                }else if (rotate < minRotation){
+                    rotate = minRotation
+                }else{
+                    rotate - rotate
+                }
+
+
+
+                invalidate()
+
+
+            }
+
+            MotionEvent.ACTION_UP ->{
+                position = rotate
+
+            }
+        }
+
+
+
+        return true
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas?.rotate(rotate, width/2f, height/2f)
+
+        paint.color = Color.BLACK
+        paint2.color = Color.RED
+
+       canvas?.drawCircle(width/2.0f, height/2f, 350f, paint)
+        canvas?.drawCircle(width/3.0f, height/2.0f, 50f, paint2)
+
+
+
+
+    }
 }
